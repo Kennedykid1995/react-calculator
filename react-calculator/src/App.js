7857e84dea22d64eb8d32fb51f4c4b8f221290e2
keith Kennedy <keithkennedy@keiths-MacBook-Air.local>
2018-11-21 13:07:07 -0600

    working on the logic
    I've gotten it to do the basic things, just having a problem with the long numbers

--- react-calculator/src/App.js
+++ react-calculator/src/App.js
@@ -5,6 +5,7 @@ import styled from 'styled-components';
 const Page = styled.div`
   display: flex; 
   justify-content: center;
+  font-family: 'Quicksand', sans-serif;
 `
 
 const Calculator = styled.div`
@@ -23,6 +24,11 @@ const OutputBox = styled.div`
   border: 1px solid white; 
   border-radius: 20px;
   margin: 10px;
+  display: flex; 
+  justify-content: flex-end;
+  align-items: center;
+  font-size: 25px;
+  overflow: hidden;
 `
 
 const NumBtnContainer = styled.div`
@@ -90,16 +96,87 @@ const ZeroBtn = styled.div`
 `
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      operator: '',
+      current: '',
+      total: '', 
+    }
+    this.handleType = this.handleType.bind(this);
+    this.calculate = this.calculate.bind(this);
+    this.handleAction = this.handleAction.bind(this);
+    this.getResult = this.getResult.bind(this);
+  }
+
+  handleType (e) {
+    const value = e.target.attributes.getNamedItem('data-filter').value;
+
+    this.setState((prevState) => {
+      const current = (this.state.current == 0 || this.state.current == this.state.total) ? '' : this.state.current;
+      return {current: parseInt(current + value)};
+    })
+  }
+  calculate (sign, number){
+    const total = this.state.total;
+    console.log(total);
+    switch(sign){
+      case "-":
+        return total - number;
+          break;
+      case "+":
+        return total + number;
+          break;
+      case "*":
+        return total * number;
+          break;
+      case "/":
+        return total / number;
+         break;
+      default:
+        return 0;
+    }
+  };
+
+  handleAction (e) {
+    const operator = e.target.attributes.getNamedItem('data-filter').value;
+
+    this.setState((prevState) => {
+      return {
+        total: prevState.current,
+        operator: operator,
+        current: 0
+      }
+    });
+    console.log(this.state);
+  }
+  getResult () {
+    this.setState({
+      current: parseFloat(this.calculate(this.state.operator, this.state.current))
+    });
+  }
+
+  cancel = () => {
+    this.setState({
+       total: '',
+       current: '',
+       operator: ''
+    })
+ }
+ 
+
+  
   render() {
     return (
       <Page>
         <Calculator>
           <NumBtnContainer>
           <OutputBox>
+            <h2>{this.state.current}</h2>
           </OutputBox>
           </NumBtnContainer>
           <NumBtnContainer>
-            <Button>
+            <Button onClick={this.cancel}>
               C
             </Button>
             <Button>
@@ -108,60 +185,60 @@ class App extends Component {
             <Button>
               %
             </Button>
-            <Button>
+            <Button data-filter="/" onClick={this.handleAction}>
               /
             </Button>
           </NumBtnContainer>
           <NumBtnContainer>
-            <Button>
+            <Button data-filter="7" onClick={this.handleType}>
               7
             </Button>
-            <Button>
+            <Button data-filter="8" onClick={this.handleType}>
               8
             </Button>
-            <Button>
+            <Button data-filter="9" onClick={this.handleType}>
               9
             </Button>
-            <Button>
-              X
+            <Button data-filter="*" onClick={this.handleAction}>
+              *
             </Button>
           </NumBtnContainer>
           <NumBtnContainer>
-            <Button>
+            <Button data-filter="4" onClick={this.handleType}>
               4
             </Button>
-            <Button>
+            <Button data-filter="5" onClick={this.handleType}>
               5
             </Button>
-            <Button>
+            <Button data-filter="6" onClick={this.handleType}>
               6
             </Button>
-            <Button>
+            <Button data-filter="-" onClick={this.handleAction}>
               -
             </Button>
           </NumBtnContainer>
           <NumBtnContainer>
-            <Button>
+            <Button data-filter="1" onClick={this.handleType}>
               1
             </Button>
-            <Button>
+            <Button data-filter="2" onClick={this.handleType}>
               2
             </Button>
-            <Button>
+            <Button data-filter="3" onClick={this.handleType}>
               3
             </Button>
-            <Button>
+            <Button data-filter="-" onClick={this.handleAction}>
               +
             </Button>
           </NumBtnContainer>
           <NumBtnContainer>
-            <ZeroBtn>
+            <ZeroBtn data-filter="0" onClick={this.handleType}>
               0
             </ZeroBtn>
             <Button>
               .
             </Button>
-            <Button>
+            <Button onClick={this.getResult}>
               =
             </Button>
           </NumBtnContainer>
